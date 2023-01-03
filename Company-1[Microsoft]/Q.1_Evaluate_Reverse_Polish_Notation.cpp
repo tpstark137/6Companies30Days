050ed6ba7dace738e8382b0963abd3b29ec2cb13
Tusshar Paul <96624900+tussharpaul13@users.noreply.github.com>
2023-01-04 00:42:34 +0530

    Q.1
    You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.

---
+++ Company-1[Microsoft]/Q.1_Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<long long>st;
+        for(auto t:tokens){
+            if(t!="+" and t!="-" and t!="*" and t!="/"){
+                st.push(stoi(t));
+            }else{
+                long long a=st.top();
+                st.pop();
+                long long b=st.top();
+                st.pop();
+                long long c;
+                if(t=="+"){
+                    c=a+b;
+                }else
+                if(t=="-"){
+                    c=b-a;
+                }else
+                if(t=="*"){
+                    c=a*b;
+                }else
+                if(t=="/"){
+                    c=b/a;
+                }
+                st.push(c);
+            }
+        }
+        return (int)st.top();
+    }
+};
