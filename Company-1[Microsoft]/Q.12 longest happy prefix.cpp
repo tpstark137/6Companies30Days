d7580e2a5b42f479fe831e1bb3dd9e400bf551fc
Tusshar Paul <96624900+tussharpaul13@users.noreply.github.com>
2023-01-05 22:05:16 +0530

    Create Q.12 longest happy prefix.cpp

---
+++ Company-1[Microsoft]/Q.12 longest happy prefix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    //KMP algorithm very important
+public:
+    string longestPrefix(string s) {
+        int n = s.size();
+        //creating lps array using kmp algorithm
+        int i=1,j=0;
+        vector<int> lps(n,0);
+        while(i<n){
+            if(s[i] == s[j]){
+                 lps[i] = j + 1;
+                 i++;
+                 j++;
+            }
+            else{
+                if(j > 0){
+                    j = lps[j-1];
+                }
+                else{
+                    lps[i] == 0;
+                    i++;
+                }
+            }
+        }
